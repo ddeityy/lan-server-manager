df3fc59a05df0a69197cc204b24245fb4415e2da
ddeityy <ddeityy@gmail.com>
2026-08-02 17:26:20 +0300

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+auto_envsubst() {
+  local template_dir="${SERVER_DIR}/tf/cfg"
+  local suffix=".template"
+
+  find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
+    output_file="${template%$suffix}"
+    envsubst < "${template}" > "${output_file}"
+  done
+}
+
+faketty() {
+  # https://stackoverflow.com/questions/1401002/how-to-trick-an-application-into-thinking-its-stdout-is-a-terminal-not-a-pipe/60279429#60279429
+  tmp=$(mktemp)
+  [ "$tmp" ] || return 99
+  cmd="$(printf '%q ' "$@")"'; echo $? > '$tmp
+  script -qfc "/bin/sh -c $(printf "%q " "$cmd")" /dev/null
+  [ -s $tmp ] || return 99
+  err=$(cat $tmp)
+  rm -f $tmp
+  return $err
+}
+
+quit() {
+  echo "*** Stopping ***"
+  "${SERVER_DIR}/rcon" -H ${IP/0.0.0.0/127.0.0.1} -p ${PORT} -P ${RCON_PASSWORD} quit
+  sleep 5
+  exit 0
+}
+
+trap 'quit' SIGTERM
+
+auto_envsubst
+
+TICKRATE_FLAGS=()
+if [ -n "${TICKRATE}" ]; then
+  case "${TICKRATE}" in
+    100)
+      echo "[Tickrate-Selector] Setting High-Performance 100 Tickrate mode..."
+      TICKRATE_FLAGS=("-tickrate" "100" "+sv_maxupdaterate" "100" "+sv_minupdaterate" "100" "+sv_maxcmdrate" "100" "+sv_mincmdrate" "100")
+      ;;
+    133)
+      echo "[Tickrate-Selector] Setting Competitive 133 Tickrate mode..."
+      TICKRATE_FLAGS=("-tickrate" "133" "+sv_maxupdaterate" "133" "+sv_minupdaterate" "133" "+sv_maxcmdrate" "133" "+sv_mincmdrate" "133")
+      ;;
+    200)
+      echo "[Tickrate-Selector] WARNING: Setting EXTREME 200 Tickrate mode. Ensure CPU can handle this!"
+      TICKRATE_FLAGS=("-tickrate" "200" "+sv_maxupdaterate" "200" "+sv_minupdaterate" "200" "+sv_maxcmdrate" "200" "+sv_mincmdrate" "200")
+      ;;
+    *)
+      echo "[Tickrate-Selector] Unknown TICKRATE='${TICKRATE}'. Falling back to default engine settings."
+      ;;
+  esac
+else
+  echo "[Tickrate-Selector] TICKRATE variable is not set. Using default 66.7 tickrate."
+fi
+
+# enablefakeip switch
+if [ "$ENABLE_FAKE_IP" = "1" ]; then
+  FAKE_IP_FLAG="-enablefakeip"
+else
+  FAKE_IP_FLAG=""
+fi
+
+faketty $SERVER_DIR/$SRCDS_EXEC \
+  -game tf \
+  -secured \
+  $FAKE_IP_FLAG \
+  "${TICKRATE_FLAGS[@]}" \
+  -steam_dir ${HOME}/.steam/steamcmd \
+  -steamcmd_script ${HOME}/tf2.txt \
+  -autoupdate \
+  +sv_setsteamaccount ${SERVER_TOKEN} \
+  -ip ${IP} \
+  -port ${PORT} \
+  +clientport ${CLIENT_PORT} \
+  -steamport ${STEAM_PORT} \
+  +tv_port ${STV_PORT} \
+  -norestart \
+  $@ & wait ${!}
